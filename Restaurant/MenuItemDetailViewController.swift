d35332a157c2fbb1b44fffc7ce5e357480965c8a
Anthony <a.mcc27@yahoo.com>
2019-05-04 03:37:26 -0500

    finished MenuTableViewController setup

--- Restaurant/MenuItemDetailViewController.swift
+++ Restaurant/MenuItemDetailViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
+    var menuItem: MenuItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +17,7 @@ class MenuItemDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+   
 
     /*
     // MARK: - Navigation
