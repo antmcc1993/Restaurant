c18024aa702ced400898802dfe004ea0c44bea6e
Anthony <a.mcc27@yahoo.com>
2019-05-05 18:37:37 -0500

    Pass data to orderTableview

--- Restaurant/MenuItemDetailViewController.swift
+++ Restaurant/MenuItemDetailViewController.swift
@@ -9,15 +9,39 @@
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
     var menuItem: MenuItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addToOrderButton.layer.cornerRadius = 5
+        updateUI()
+        
     }
     
-   
+    func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        detailTextLabel.text = menuItem.detailText
+    }
+    
+    
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
+        
+        MenuController.shared.order.menuItems.append(menuItem)
+    }
+    
 
     /*
     // MARK: - Navigation
